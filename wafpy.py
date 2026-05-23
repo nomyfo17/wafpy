851a4c82d0f6ba314d66b69a1f401f835835dc20
n17ultra <nomyfo171234@gmail.com>
2026-05-23 09:34:02 -0700

    Enhance randomgame with user input for max number
    Refactor randomgame function to allow user-defined max number and implement guessing logic.

--- wafpy.py
+++ wafpy.py
@@ -1,9 +1,28 @@
 import time
 import os
-import randomgame
+import random
+import time
 
 def randomgame():
-    randomgame.start()
+    nr = input("number max = ")
+    rnr = int (nr)
+    number = random.randint (0,rnr)
+    guesses = 0
+    while True:
+        print("guess the number")
+        ng = input()
+        rng = int (ng)
+        if rng == number:
+            gs = str (guesses)
+            print("You Got Is it was " + ng)
+            print("your guesses was " + gs)
+            time.sleep(10)
+            break
+        elif rng < number:
+            print("nice try but its higher")
+        elif rng > number:
+            print("nice try but its lower")
+        guesses = guesses + 1
 
 loc = []
 
@@ -128,3 +147,6 @@ def name():
                 file.write(str(item) + "\n")
         print("file sussecfullly saved!")
     names2()
+
+#use these funcutions to make your own waf
+
